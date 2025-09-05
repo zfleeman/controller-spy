e6ca22677e354bf235bb1c969779d8a7b9ffec08
Zach Fleeman <zfleeman@gmail.com>
2025-09-04 21:11:23 -0600

    display error

--- controller_overlay.py
+++ controller_overlay.py
@@ -93,4 +93,8 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Error: {e}")
+        input("Press Enter to exit...")
